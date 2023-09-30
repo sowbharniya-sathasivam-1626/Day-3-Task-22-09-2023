12f62370307ad4be153f5751d576850ebecf8b3a
sowbharniya-sathasivam-1626 <sowbhargeo2000@gmail.com>
2023-09-30 16:41:02 +0530

    Made some changes in script2.js

--- script2.js
+++ script2.js
@@ -1,10 +1,18 @@
-var obj1 = {name: "Person 1", age: 5};
-var obj2 = {age: 5, name: "Person 1"};
-var Obj1 = JSON.stringify(obj1);
-var Obj2 = JSON.stringify(obj2);
-
-if (Obj1 == Obj2){
-    console.log("Obj1 and Obj2 are the Same Objects");
-} else {
-    console.log("Obj1 and Obj2 are not the Same Objects");
+var obj1= {name: "Sowbharniya", age: 24};
+var obj2 = {age: 24, name: "Sowbharniya"};
+var result=true;
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            result=false;
+            break;
+        }
+    }
 }
+else {
+    result=false;
+}
+console.log(result);
